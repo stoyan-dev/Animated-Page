bbbd6274f1fff5d287b7cc82bfb299f3208a59b0
stoyan-dev <xuij777@gmail.com>
2024-01-05 13:39:38 +0200

    Making the animations and replacing the images

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ const totalSlides = slides.length;
 let index = 0;
 
 rotateBtn.addEventListener('click', ()=>{
-    slides[index].classList.remove('active')
+    rotateBtn.classList.add('active')
+    setTimeout(()=> {{
+    rotateBtn.classList.remove('active')
+    }}, 2100)
 
+    slides.forEach(slide => {
+        if(slide.classList.contains('active')){
+            slide.classList.add('after-active')
+        }
+        else {
+            slide.classList.remove('after-active')
+        }
+    })
+
+    slides[index].classList.remove('active')
     index++
 
     if(index == totalSlides){
